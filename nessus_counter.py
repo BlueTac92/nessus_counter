b5c150e714d774fe0d4e8fc5d06555f5d2af89ac
AERIANDI\JohnB <john.brown@aeriandi.com>
2016-09-13 17:01:14 +0100

    added uniq vulnerability count

--- nessus_counter.py
+++ nessus_counter.py
@@ -3,11 +3,9 @@ import csv
 
 print('----------------------------------')
 print('     Nessus counting tool')
-print('')
-print('     GPLv3 2016')
-print('')
-print('')
-print('     by John Brown')
+print('----------------------------------')
+print('  -   GPLv3 2016')
+print('  -   by John Brown')
 print('----------------------------------')
 
 #inputPath = input("Please enter to path to the directory containing your nessus reports: ")
@@ -19,38 +17,63 @@ print('----------------------------------')
 #print ("Default Output Path: %s" % outputPath)
 #
 
-nessus_report = "./test_data/scan_data.csv"
+nessus_report = "Monthly_Scheduled_Site_1_BE_Scan_idi7p6.csv"
 
 # not exactly the right idea for lists, but consider converting to a list value
 sev = ['critical', 'high', 'medium', 'low']
+
+uniqueVulns = []
+critHosts = []
+highHosts = []
+mediumHosts = []
+lowHosts = []
+noneHosts = []
 
 critical = 0
 high = 0
 medium = 0
 low = 0
+none = 0
 
-
+# Get a total count of each criticality
 # if are repeated a lot, get rid of this shit
 #
-
 with open(nessus_report, newline='') as csvfile:
-    vulns = csv.DictReader(csvfile)
-    for row in vulns:
-        if row['severity'] == "critical":
+    report = csv.DictReader(csvfile)
+    for row in report:
+        if row['Risk'] == "Critical":
             critical = critical + 1
-        if row['severity'] == "high":
+        if row['Risk'] == "High":
             high = high + 1
-        if row['severity'] == "medium":
+        if row['Risk'] == "Medium":
             medium = medium + 1
-        if row['severity'] == "low":
+        if row['Risk'] == "Low":
+            low = low + 1
+        if row['Risk'] == "None":
             low = low + 1
 
 print ('Total vulnerablilities')
 print ('----------------------------------')
 print ('Critical:   %i' % critical)
 print ('High:       %i' % high)
-print ('Meidum:     %i' % medium)
+print ('Medium:     %i' % medium)
 print ('Low:        %i' % low)
+print ('None:       %i' % none)
+
+# Count total number of unique vulnerablilities
+
+with open(nessus_report, newline='') as csvfile:
+    report = csv.DictReader(csvfile)
+    for row in report:
+        if row['Name'] not in uniqueVulns:
+            uniqueVulns.append(row['Name'])
+print ('----------------------------------')
+print('Total number of uniq vulnerablilities: ' + str(len(uniqueVulns)))
+print ('----------------------------------')
+
+
+
+
 
 
 
